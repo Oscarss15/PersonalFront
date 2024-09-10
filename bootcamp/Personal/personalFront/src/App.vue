ca6a732ea200207fa85e6a9b2446bee92a07984b
Oscarss15 <117840754+Oscarss15@users.noreply.github.com>
2024-09-10 22:56:35 +0200

    creacion footer

--- bootcamp/Personal/personalFront/src/App.vue
+++ bootcamp/Personal/personalFront/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
@@ -10,6 +11,11 @@ import Navbar from "./components/Navbar.vue";
   <main></main>
 
   <RouterView />
+  <Footer></Footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  height: 318px;
+}
+</style>
---
+++ bootcamp/Personal/personalFront/src/components/Footer.vue
@@ -0,0 +1,110 @@
+<script setup></script>
+
+<template>
+  <footer class="text-center text-lg-start" style="background-color: #2d3b57">
+    <div class="container d-flex justify-content-center py-5">
+      <button
+        type="button"
+        class="btn btn-primary btn-lg btn-floating mx-2"
+        id="btnfb"
+      >
+        <i class="fab fa-facebook-f"></i>
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary btn-lg btn-floating mx-2"
+        id="btntw"
+      >
+        <i class="fab fa-twitter"></i>
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary btn-lg btn-floating mx-2"
+        id="btnig"
+      >
+        <i class="fab fa-instagram"></i>
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary btn-lg btn-floating mx-2"
+        id="btnws"
+      >
+        <i class="fab fa-whatsapp"></i>
+      </button>
+    </div>
+
+    <div
+      class="text-center p-3"
+      style="background-color: #404040"
+      id="divfooter"
+    >
+      © 2024 Copyright:
+      <a
+        class="linkProyecto"
+        href="https://github.com/FactoriaF5-FactZoopia/Sonar-Events-FrontEnd.git"
+        target="_blank"
+        >PadelMatch</a
+      >
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  box-shadow: 0 -10px 15px #b0fc33;
+}
+
+button {
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  border: 2px solid #b0fc33;
+  box-shadow: 0 0 5px #b0fc33;
+  background-color: #2d3b57;
+  transition: transform 0.3s;
+}
+
+i {
+  color: #b0fc33;
+}
+
+button:hover i {
+  color: #2d3b57;
+}
+
+#btnfb:hover,
+#btntw:hover,
+#btnig:hover,
+#btnws:hover {
+  transform: scale(1.1);
+  background-color: #b0fc33;
+  box-shadow: 0 0 15px #b0fc33;
+  border: 2px solid #b0fc33;
+  transform: rotateY(360deg);
+}
+
+#btnws,
+#btnig,
+#btntw,
+#btnfb {
+  transition: transform 3s;
+  transform-style: preserve-3d;
+}
+
+#divfooter {
+  color: white;
+  box-shadow: 0 0 15px #b0fc33;
+}
+
+.linkProyecto {
+  font-size: 18px;
+  color: #b0fc33;
+}
+
+@media (max-width: 600px) {
+  button {
+    width: 60px;
+    height: 60px;
+  }
+}
+</style>
