91286a9e56cd8df09590fd0f6a015bf0af77e416
Oscarss15 <117840754+Oscarss15@users.noreply.github.com>
2024-10-04 23:21:45 +0200

    añadido loading

--- bootcamp/Personal/personalFront/src/main.js
+++ bootcamp/Personal/personalFront/src/main.js
@@ -1,12 +1,29 @@
 import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+
+// Mostrar la pantalla de carga antes de cada cambio de ruta
+router.beforeEach((to, from, next) => {
+  const loadingElement = document.getElementById("loading");
+  if (loadingElement) {
+    loadingElement.classList.remove("hide"); // Mostrar el loading
+  }
+  next(); // Continuar con la navegación
+});
+
+// Ocultar la pantalla de carga después de que la ruta haya cambiado
+router.afterEach(() => {
+  const loadingElement = document.getElementById("loading");
+  if (loadingElement) {
+    setTimeout(() => {
+      loadingElement.classList.add("hide"); // Ocultar el loading con un pequeño retardo
+    }, 1500); // Puedes ajustar este tiempo si es necesario
+  }
+});
 
 app.use(createPinia());
 app.use(router);
